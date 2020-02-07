30be1fb7cc20d97ce512426a9a34e3555c05545c
LewisKaye <lewiskaye@hotmail.co.uk>
2020-02-07 14:57:52 +0000

    Finished First 9 Statements

--- src/main/java/com/napier/sem/sql-statements.sql
+++ src/main/java/com/napier/sem/sql-statements.sql
@@ -2,31 +2,31 @@
 --  TODO Is there a way to give each statement a tag, and you can import each statement using a find by tag?
 
 --All the countries in the world organised by largest population to smallest.
-SELECT `Name` FROM `country` ORDER BY `population` Desc
+SELECT `Name`, `Population` FROM `country` ORDER BY `Population` Desc
 
 --All the countries in a continent organised by largest population to smallest.
-SELECT `Name` FROM `country` GROUP BY `Continent` ORDER BY `Population` Desc
+SELECT `Name`, `Population` FROM `country` GROUP BY `Continent` ORDER BY `Population` Desc
 
 --All the countries in a region organised by largest population to smallest.
-SELECT `Name` FROM `country` GROUP BY `Region` ORDER BY `Population` Desc
+SELECT `Name`, `Population` FROM `country` GROUP BY `Region` ORDER BY `Population` Desc
 
 --The top N populated countries in the world where N is provided by the user.
-
+SELECT `Name`, `Population` FROM `country` ORDER BY `Population` Desc LIMIT 0,[user input]
 
 --The top N populated countries in a continent where N is provided by the user.
-
+SELECT `Name`, `Population` FROM `country` GROUP BY `Continent` ORDER BY `Population` Desc LIMIT 0,[user input]
 
 --The top N populated countries in a region where N is provided by the user.
-
+SELECT `Name`, `Population` FROM `country` GROUP BY `Region` ORDER BY `Population` Desc LIMIT 0,[user input]
 
 --All the cities in the world organised by largest population to smallest.
-
+SELECT `Name`, `Population` FROM `city` ORDER BY `Population` Desc
 
 --All the cities in a continent organised by largest population to smallest.
-
+SELECT `Name`, `Population` FROM `city` GROUP BY `Continent` ORDER BY `Population` Desc
 
 --All the cities in a region organised by largest population to smallest.
-
+SELECT `Name`, `Population` FROM `city` GROUP BY `Region` ORDER BY `Population` Desc
 
 --All the cities in a country organised by largest population to smallest.
 
