76591a3f800bb0b0c80bdbdff2bf6842098e3b12
Wez-Young <oldwezyoung@gmail.com>
2020-02-26 16:35:13 +0000

    Adding initial methods for cities sql statements to CitiesLargeToSmall

--- src/main/java/com/napier/sem/CitiesData.java
+++ src/main/java/com/napier/sem/CitiesData.java
@@ -1,4 +1,63 @@
 package com.napier.sem;
 
 public class CitiesData {
+
+
+    private int id;
+    private String name;
+    private String countryCode;
+    private String district;
+    private int population;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+    public int getPopulation() {
+        return population;
+    }
+
+    //Override .toString() to print data needed
+    public String toString(String parameter){
+        String data ="";
+        //if statements to check what data is needed and to be appended on to string
+        if(parameter.contains("name"))
+        {
+            data += name;
+            data += ", ";
+        }
+        if(parameter.contains("population"))
+        {
+            data += population;
+            data += ", ";
+        }
+        return data;
+    }
 }
--- src/main/java/com/napier/sem/CitiesLargeToSmall.java
+++ src/main/java/com/napier/sem/CitiesLargeToSmall.java
@@ -1,4 +1,65 @@
 package com.napier.sem;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class CitiesLargeToSmall {
+
+    public CitiesData getCitiesLargeToSmallInWorld(Connection con)
+    {
+        try{
+            //create an SQL statement
+            Statement stmt = con.createStatement();
+            //Create string for SQL statement
+            String strSelect =
+                    "SELECT `Name`, `Population`" +
+                            " FROM `city`" +
+                            " ORDER BY `Population` Desc";
+            //execute statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+
+            //Return country if valid
+            //check one is returned
+            while(rset.next())
+            {
+                CountriesData country = new CountriesData();
+                //Result set gets name and population data
+                country.name = rset.getNString("Name");
+                country.population = rset.getInt("population");
+                //Puts data into one string to be displayed
+                System.out.println(country.toString("name, population"));
+            }
+
+        }
+        catch(Exception e)
+        {
+            //error message displays if unable to get country data
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get country details");
+            return null;
+        }
+
+        return null;
+    }
+
+    public CitiesData getCitiesLargeToSmallInContinent(Connection con)
+    {
+        return null;
+    }
+
+    public CitiesData getCitiesLargeToSmallInRegion(Connection con)
+    {
+        return null;
+    }
+
+    public CitiesData getCitiesLargeToSmallInCountry(Connection con)
+    {
+        return null;
+    }
+
+    public CitiesData getCitiesLargeToSmallInDistrict(Connection con)
+    {
+        return null;
+    }
 }
