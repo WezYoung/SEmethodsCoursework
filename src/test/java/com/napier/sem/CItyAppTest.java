911edaaa8a91974b535831a90f28f750ccebb834
Lewis Kaye <lewiskaye@hotmail.co.uk>
2020-03-16 15:42:05 +0000

    Unit Test Updates

--- src/test/java/com/napier/sem/CItyAppTest.java
+++ src/test/java/com/napier/sem/CItyAppTest.java
@@ -8,7 +8,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
-
+import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CItyAppTest {
