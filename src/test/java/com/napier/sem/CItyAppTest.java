56191163d459469f4e4bc6a218f0d89388314b00
NateG00 <56965176+nateg00@users.noreply.github.com>
2020-03-16 14:50:50 +0000

    Added comments to City app Test

--- src/test/java/com/napier/sem/CItyAppTest.java
+++ src/test/java/com/napier/sem/CItyAppTest.java
@@ -13,18 +13,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class CItyAppTest {
 
+    //Test 1, tests city get name
     @Test
     public void testCityReportNameSuccess(){
         City city = new City("London");
         assertEquals("London", city.getName());
     }
 
+    //Test 2, tests failure when cities are not equal
     @Test
     public void testCityReportNameFail(){
         City city = new City("Madrid");
         assertEquals(city.getName(), "Barcelona");
     }
 
+    //Test 3, tests population get
     @Test
     public void testCityReportPopulation(){
         City city = new City("Madrid");
